887e4db3860573894fc1698d4bb590455642b098
molchanov.s <stepanmol4anoff@yandex.ru>
2023-04-05 23:08:50 +0300

    отредактирована заготовка, готовы вход и выход из аккаунта

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import datetime as dt
 from flask import Flask, render_template, redirect
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_required, login_user, logout_user
 from data import db_session
 from data.users import User
 from forms.user import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = dt.timedelta(days=365)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -38,6 +40,26 @@ def register():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login/', methods={'GET', 'POST'})
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html', message='Неправильный логин или пароль', form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
 
 
 @app.route('/')
